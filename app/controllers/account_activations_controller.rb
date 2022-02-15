172fc3c44a83bfaf86cdbb575234137bb5b939d6
JosephN37 <josephnathanael.2014@gmail.com>
2022-02-15 03:52:50 +0000

    add debug print message

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,6 +3,8 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      puts "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
+      puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
       user.activate
       log_in user
       flash[:success] = "Account activated!"
